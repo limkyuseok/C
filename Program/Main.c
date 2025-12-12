3360e3eec220f7a6c89c2c95f6fd7bbb62d5e305
limkyuseok <lgkk50025@gmail.com>
2025-12-12 21:43:21 +0900

    (2025.12.12)상수

--- Program/Main.c
+++ Program/Main.c
@@ -3,73 +3,101 @@
 
 void main()
 {
-#pragma region 변수
-	// 데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+#pragma region 산술 연산자
+
+	//1. 변수 = 변수 + 변수
+
+	// 2. 변수 = 변수 - 리터럴 상수
+
+	// 3. 변수 = 변수 * 심볼릭 상수
+
+	// 4. 변수 = 리터럴 상수 / 리터럴 상수
+
+	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+
 	
-#pragma region 자료형
-	// 데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다.
+	/*int a = 3;
+	int b = 5;
+	int c = a + b;
 
-	// 자료형은 각각의 자료형마다 크기가 정해져 있으며, 자료형의
-	// 크기는 바이트 단위로 이루어져 있으며, 자료형의 경우 자료형에
-	// 따라 저장할 수 있는 값의 종류와 범위가 결정됩니다.
+	printf("c의 값 : %d\n", c);
 
-#pragma region 변수의 이름규칙
+	int d = 10;
+	int e = d - 4;
+
+	printf("e의 값 : %d\n", e);
+
+	int ti = 5;
+	int x = 3;
+	int y = ti * 3;
+
+	printf("y의 값: %d\n", y);
+
+
+	int j = 21 / 7;
+
+	printf("j의 값 : %d\n", j);
+
+	int pi = 3.1415;
+		
+	int o = pi % 2;
+
+	printf("o의 값 :  %d\n", o);*/
+
+	/*int storage = 0;
+
+	int x = 10;
+	int y = 5;
+	int z = 1;
+	int w = 10;
+
+	const int constant = 5;
+
+	storage = x + y;
+
+	printf("storage : %d\n", storage);
+
+	storage = 20 / 5;
+
+	printf("storage : %d\n", storage);
 	
-	// 1. 변수의 이름은 대소문자를 구분합니다.
+	storage = w * constant;
 
-	/* ex) int health;
-	 ex) int HEALTH;*/
+	printf("storage : %d\n", storage);
 
-	// 2. 변수의 이름으로 예약어를 사용할 수 없습니다.
-	// ex) char char;
+	storage = constant % 5;
 
-	// 3. 변수의 이름에는 공백이 포함될 수 없습니다.
-	// ex) float time Delta;
-
-	// 4. 변수의 이름으로 특수 문자 "_", "$"만 사용할 수 있습니다.
-	// ex)short under_score;
-	// ex)long currency$;
-
-	// 5. 변수의 이름은 숫자로 시작할 수 없습니다.
-	/*int year2025;
-	int 5day;*/
-
-
+	printf("storage : %d\n", storage);*/
 
 #pragma endregion
 
+#pragma region 비트
+	//데이터를 나타내는 최소의 단위이며, 0 또는 1의
+	//조합으로 논리 계산을 수행한다.
+
+#pragma region (10)진수를 (2)진수로 변환하는 과정
+	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
+	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+
+	//23(10)
+	//2 - 11 (-1)
+	//2 - 5 (-1)
+	//2 - 2 (-1)
+	//2 - 1 (0)
+	// 10111 (2)
+
 #pragma endregion
-#pragma region 서식 지정자
-	//변수의 값을 출력하기 위한 자료형의 정보를 명시적으로
-	// 지정해주는 것입니다.
 
-	char character = 'A';
-	
-	int integer = 10;
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
 
-	float decimal = 5.5f;
-
-
-	//서식 지정자의 경우 하나의 표준 출려 함수에 여러 개의
-	//서식을 넣을 수 있으며, 서식 지정자의 순서는 왼쪽에서 부터
-	//시작합니다.
-
-	printf("character : %c\ninteger : %d\ndecimal : %f", character, integer, decimal);
-
-	/*서식 지정자는 각각의 서식에 따라 원하는 데이터를 출력할 수 있으며,
-	서식과 변수의 형태가 일차하지 않으면 원하는 값이 
-	출력되지 않는다.*/
+	//각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
+	//다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
 
 
 #pragma endregion
 
 
-
 #pragma endregion
-
-
-
-
-
 
 }
