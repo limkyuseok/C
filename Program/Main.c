427190537e2ee6c9e2edb2bd726d99f80f9a6bdb
limkyuseok <lgkk50025@gmail.com>
2025-12-15 21:43:29 +0900

    (2025.12.15) 비트

--- Program/Main.c
+++ Program/Main.c
@@ -3,100 +3,40 @@
 
 void main()
 {
-#pragma region 산술 연산자
+#pragma region 오버플로우
+	//특정한 자료형이 표현할 수 있는 최댓값의 범위를
+	//넘어서 연산을 수행하는 과정입니다.
 
-	//1. 변수 = 변수 + 변수
+	/*char storage = 127 + 3;
 
-	// 2. 변수 = 변수 - 리터럴 상수
+	printf("storage = %d\n", storage);*/
 
-	// 3. 변수 = 변수 * 심볼릭 상수
-
-	// 4. 변수 = 리터럴 상수 / 리터럴 상수
-
-	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
-
-	
-	/*int a = 3;
-	int b = 5;
-	int c = a + b;
-
-	printf("c의 값 : %d\n", c);
-
-	int d = 10;
-	int e = d - 4;
-
-	printf("e의 값 : %d\n", e);
-
-	int ti = 5;
-	int x = 3;
-	int y = ti * 3;
-
-	printf("y의 값: %d\n", y);
-
-
-	int j = 21 / 7;
-
-	printf("j의 값 : %d\n", j);
-
-	int pi = 3.1415;
-		
-	int o = pi % 2;
-
-	printf("o의 값 :  %d\n", o);*/
-
-	/*int storage = 0;
-
-	int x = 10;
-	int y = 5;
-	int z = 1;
-	int w = 10;
-
-	const int constant = 5;
-
-	storage = x + y;
-
-	printf("storage : %d\n", storage);
-
-	storage = 20 / 5;
-
-	printf("storage : %d\n", storage);
-	
-	storage = w * constant;
-
-	printf("storage : %d\n", storage);
-
-	storage = constant % 5;
-
-	printf("storage : %d\n", storage);*/
+	//오버플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	//실수일 때 오버플로우가 발생하면 infinity라는 값으로 출력됩니다.
 
 #pragma endregion
 
-#pragma region 비트
-	//데이터를 나타내는 최소의 단위이며, 0 또는 1의
-	//조합으로 논리 계산을 수행한다.
+#pragma region 언더플로우
+	//특정한 자료형이 표현할 수 있는 최솟값의 범위를
+	//넘어서 연산을 수행하는 과정입니다.
 
-#pragma region (10)진수를 (2)진수로 변환하는 과정
-	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
-	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+	/*char data = -128 - 6;
 
-	//23(10)
-	//2 - 11 (-1)
-	//2 - 5 (-1)
-	//2 - 2 (-1)
-	//2 - 1 (0)
-	// 10111 (2)
+	printf("data 변수의 값 : %d\n", data);*/
+
+	//언더플로우는 부호 없는 자료형에서도 똑같이 발생하며,
+	//실수일 때 언더플로우가 발생하면 0이라는 값으로 출력합니다.
 
 #pragma endregion
 
-#pragma region (2)진수를 (10)진수로 변환하는 과정
-	// 1byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
+#pragma region 부호없는 자료형
 
-	//각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
-	//다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
+	//unsigned int count = 4294967295;
 
-
-#pragma endregion
-
+	//[1111 1111] [1111 1111] [1111 1111] [1111 1111]
+	//printf("signed count 변수의 값 : %d\n", count);
+	//[1111 1111] [1111 1111] [1111 1111] [1111 1111]
+	//printf("unsigned count 변수의 값 : %u\n", count);
 
 #pragma endregion
 
